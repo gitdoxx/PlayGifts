d32d4bd35a5a26b9893d3e9603e00ac1e0bd6295
unlinked <alex.petrov4728@gmail.com>
2025-10-20 23:39:34 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8376983442:AAHmGgI7IgrocUexNu0sTqSLlHQ4wmaMpVc"
-WEBAPP_URL = "https://YOUR_USERNAME.github.io/PlayGifts/webapp/"  # ЗАМЕНИТЕ YOUR_USERNAME на ваш!
+WEBAPP_URL = "https://gitdoxx.github.io/PlayGifts/webapp/"  # ЗАМЕНИТЕ YOUR_USERNAME на ваш!
 DB_PATH = "gifts_bot.db"
 
 # Настройки базы данных
